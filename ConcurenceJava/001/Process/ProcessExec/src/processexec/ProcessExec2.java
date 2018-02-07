5726932a2bce901eb91dadecb2e25ecd5bdccbf8
Pascal Fares (pascalfares on CL) <pfares@cofares.net>
2018-02-07 14:09:21 +0200

    stdin, stdout, stderr

--- ConcurenceJava/001/Process/ProcessExec/src/processexec/ProcessExec2.java
+++ ConcurenceJava/001/Process/ProcessExec/src/processexec/ProcessExec2.java
@@ -13,6 +13,8 @@ package processexec;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,7 +29,7 @@ public class ProcessExec2 {
      * Un chemin pour vos execution et réféence de fichier, adaptez le a votre
      * Environement
      */
-    public static final String CHEMIN = "/home/pascalfares/tp001";
+    public static final String CHEMIN = "/home/pascalfares";
     
     /**
      * Récuperer le standard output du processus fils
@@ -46,18 +48,27 @@ public class ProcessExec2 {
     private static BufferedReader getError(Process p) {
         return new BufferedReader(new InputStreamReader(p.getErrorStream()));
     }
+    
+    private static PrintWriter getInput(Process p){
+        return new PrintWriter (new OutputStreamWriter(p.getOutputStream()));
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         System.out.println("Début du programme 2");
         try {
-            String[] commande = {"ls", "-l", "-a", CHEMIN};
+            String[] commande = {CHEMIN+"/bin/hello.sh", "Bonjour"};
             Process p = Runtime.getRuntime().exec(commande);
             BufferedReader output = getOutput(p);
             BufferedReader error = getError(p);
+            PrintWriter input  = getInput(p);
+            
             String ligne = "";
 
+            input.printf("Un texte\n");
+            input.flush();
+            
             while ((ligne = output.readLine()) != null) {
                 System.out.println(ligne);
             }
@@ -65,7 +76,7 @@ public class ProcessExec2 {
             while ((ligne = error.readLine()) != null) {
                 System.out.println(ligne);
             }
-
+            
             p.waitFor();
         } catch (IOException e) {
             e.printStackTrace();
