42889f2fb3e0716fa71c787a8c6a04ac36cccba9
Pascal Fares <pascal.fares@cofares.net>
2018-02-11 10:18:31 +0200

    Synchronized methodes de classe

---
+++ ConcurenceJava/Partage/CDDCAvecSynchroClass/src/cddc/CDDC.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cddc;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Pascal Fares
+ */
+public class CDDC {
+
+    /**
+     * Code différents Données commune
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            Data d=new Data();
+            Runnable tache1= new Tache1(d);
+            Runnable tache2= new Tache2(d);
+            
+            Thread t1=new Thread(tache1);
+            Thread t2=new Thread(tache2);
+            
+            t1.start();
+            t2.start();
+            t1.join();
+            t2.join();
+            System.out.printf("\nSe termine avce la valeur %d devrait être 200000!\n",
+                    d.getDonee());
+        } catch (InterruptedException ex) {
+            Logger.getLogger(CDDC.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
---
+++ ConcurenceJava/Partage/CDDCAvecSynchroClass/src/cddc/Tache2.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cddc;
+
+/**
+ *
+ * @author Pascal Fares
+ */
+public class Tache2 implements Runnable {
+
+    private Data referenceDonnees;
+    private int DonneePrivee;
+
+    public Tache2(Data data) {
+        referenceDonnees = data;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("debut t2");
+        while (DonneePrivee < 100000) {
+                referenceDonnees.inc();
+            DonneePrivee++;
+            //System.out.printf(" <T2 Priv : %d Glob: %d> ", 
+            //DonneePrivee, referenceDonnees.getDonee());
+        }
+        System.out.println("fin t2");
+    }
+
+}
