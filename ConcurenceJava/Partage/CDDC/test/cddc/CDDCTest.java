220ef36079ffb1ba370ce2d176567c8ea6ce49ee
Pascal Fares Domicile <pascal.fares@cofares.net>
2018-10-20 09:31:17 +0300

    Refactor comments Junit test de Code différent donnée commune sans synchronisation

--- ConcurenceJava/Partage/CDDC/test/cddc/CDDCTest.java
+++ ConcurenceJava/Partage/CDDC/test/cddc/CDDCTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cddc;
 
 import org.junit.After;
@@ -40,6 +35,7 @@ public class CDDCTest {
 
     /**
      * Test of main method, of class CDDC.
+     * @throws java.lang.InterruptedException
      */
     @Test
     public void testMain() throws InterruptedException {
