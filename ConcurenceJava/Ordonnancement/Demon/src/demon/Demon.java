3a87485c15d757e62f10325c3d3b2e69f920405c
Pascal Fares (pascalfares on CL) <pfares@cofares.net>
2018-10-21 18:47:50 +0300

    Creation runnable lambda ou classe anonyme + thread demon ou non deamon

--- ConcurenceJava/Ordonnancement/Demon/src/demon/Demon.java
+++ ConcurenceJava/Ordonnancement/Demon/src/demon/Demon.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package demon;
 
 /**
@@ -11,10 +6,12 @@ package demon;
  */
 public class Demon {
 
+    //Trois méthodes pour initialiser un Thread
     /**
-     * @param args the command line arguments
+     * par lambda
+     * @return Thread
      */
-    public static void main(String[] args) {
+    public static Thread arrange1() {
         Thread t = new Thread(() -> {
             try {
                 while (true) {
@@ -24,16 +21,44 @@ public class Demon {
                 System.out.println("Fin demon");
             }
         });
-        
-       if (args.length == 1) {
-           if (args[0].equals("demon")) t.setDaemon(true);
-           
-       }
-       else {
-           System.out.println("Lancer en donnant demon "
-                   + "ou utilisateur et voyez la différence!");
-       }
-       t.start();
+        return t;
+    }
+
+    /**
+     * Par classe anonyme (instance de Runnable)
+     * @return 
+     */
+    public static Thread arrange2() {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                try {
+                    while (true) {
+                        System.out.println("Execution demon");
+                    }
+                } finally {
+                    System.out.println("Fin demon");
+                }
+            }
+        });
+        return t;
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Thread t1 = arrange1();
+
+        if (args.length == 1) {
+            if (args[0].equals("demon")) {
+                t1.setDaemon(true);
+            }
+
+        } else {
+            System.out.println("Lancer en donnant demon "
+                    + "ou utilisateur et voyez la différence!");
+        }
+        t1.start();
     }
 
 }
