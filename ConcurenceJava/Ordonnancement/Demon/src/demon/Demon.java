18e3811358d9376cb47205df31055f847db2c558
Pascal Fares (pascalfares on CL) <pfares@cofares.net>
2018-10-21 19:19:36 +0300

    Creation runnable lambda ou classe anonyme + thread demon ou non deamon
    Nommer le thread

--- ConcurenceJava/Ordonnancement/Demon/src/demon/Demon.java
+++ ConcurenceJava/Ordonnancement/Demon/src/demon/Demon.java
@@ -11,16 +11,17 @@ public class Demon {
      * par lambda
      * @return Thread
      */
-    public static Thread arrange1() {
+    public static Thread arrange1(String name) {
         Thread t = new Thread(() -> {
             try {
                 while (true) {
-                    System.out.println("Execution demon");
+                    System.out.println("Execution de "+ Thread.currentThread());
                 }
             } finally {
                 System.out.println("Fin demon");
             }
         });
+        t.setName(name);
         return t;
     }
 
@@ -28,18 +29,19 @@ public class Demon {
      * Par classe anonyme (instance de Runnable)
      * @return 
      */
-    public static Thread arrange2() {
+    public static Thread arrange2(String name) {
         Thread t = new Thread(new Runnable() {
             public void run() {
                 try {
                     while (true) {
-                        System.out.println("Execution demon");
+                        System.out.println("Execution de "+ Thread.currentThread());
                     }
                 } finally {
                     System.out.println("Fin demon");
                 }
             }
         });
+        t.setName(name);
         return t;
     }
 
@@ -47,18 +49,20 @@ public class Demon {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Thread t1 = arrange1();
+        Thread t1 = arrange1("Tache 1 par lambda");
+        Thread t2 = arrange1("Tache 2 par classe anonyme");
 
         if (args.length == 1) {
             if (args[0].equals("demon")) {
                 t1.setDaemon(true);
+                t2.setDaemon(true);
             }
 
         } else {
             System.out.println("Lancer en donnant demon "
                     + "ou utilisateur et voyez la différence!");
         }
-        t1.start();
+        t1.start();t2.start();
     }
 
 }
